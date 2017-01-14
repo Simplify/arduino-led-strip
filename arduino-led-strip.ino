4615eb4849556b88d5c541ab488e09746aff21a0
Saša Jovanić <sasa@simplify.ba>
2017-01-14 19:24:47 +0100

    More effects, code clean-up and fixes

--- arduino-led-strip.ino
+++ arduino-led-strip.ino
@@ -13,11 +13,9 @@
 // Install FastLED library using Library manager
 #include <FastLED.h>
 
-#define DEBUG 0
-
 #define NUM_LEDS 100
 // Define the array of leds
-CRGB leds[NUM_LEDS];
+CRGBArray<NUM_LEDS> leds;//[NUM_LEDS];
 
 // Arduino pins
 #define STRIP_PIN 6     // PIN for sending data to LED strip
@@ -27,6 +25,8 @@ CRGB leds[NUM_LEDS];
 enum all_modes {
   off,
   solid_white,
+  first_half,
+  second_half,
   solid_red,
   solid_blue,
   solid_purple,
@@ -35,17 +35,18 @@ enum all_modes {
   fire,
   twinkle,
   twinkle_random,
-  twinkle_single,
   snow_sparkle,
-  // Disabled:
+  twinkle_single,
   one_at_a_time,
   bars,
   strobe,
+  rainbow_cycle,
+  rainbow_loop,
+  // Disabled:
   solid_green,
   solid_yellow,
-  sparkle,
   // Last
-  last = snow_sparkle
+  last = strobe
 };
 
 volatile all_modes run_mode = off;  // LED effects mode setting
@@ -57,13 +58,6 @@ void setup() {
   // indicator pin on:
   pinMode(INDICATOR_PIN, OUTPUT);
   digitalWrite(INDICATOR_PIN, HIGH);
-  // Setup console
-  if (DEBUG) {
-    Serial.begin(115200);
-    delay(10);
-    Serial.println();
-    Serial.println();
-  }
   // LED strip
   FastLED.addLeds<NEOPIXEL, STRIP_PIN>(leds, NUM_LEDS);
   // Mode switcher
@@ -81,10 +75,6 @@ void changeMode() {
     } else {
       run_mode = all_modes(run_mode + 1);
     }
-    if (DEBUG) {
-      Serial.print("Mode changed: ");
-      Serial.println(run_mode);
-    }
     ttime = millis();
   }
   digitalWrite(INDICATOR_PIN, state);
@@ -92,54 +82,60 @@ void changeMode() {
 
 // Make whole LED strip in single color
 void runSingleColor(CRGB color) {
-  for (int i = 0 ; i < NUM_LEDS; i++ ) {
-    leds[i] = color;
-  }
-  FastLED.show();
+  FastLED.showColor(color);
   delay(200);
 }
 
-// Loop red, green or blue single LED
+void runFirstHalf() {
+  memset(leds, 0, NUM_LEDS * 3);
+  leds(10, 39) = CRGB::White;
+  FastLED.show();
+  while (true) {
+    delay(30);
+    if (run_mode != first_half) return;
+  }
+}
+
+void runSecondHalf() {
+  memset(leds, 0, NUM_LEDS * 3);
+  leds(60, 89) = CRGB::White;
+  FastLED.show();
+  while (true) {
+    delay(30);
+    if (run_mode != second_half) return;
+  }
+}
+
+// Loop random color, one LED at the time
 void runOneAtATime() {
-  // one at a time
-  for (int j = 0; j < 3; j++) {
+  while (true) {
+    CHSV color = CHSV(random(0, 255), random(200, 255), random(200, 255));
     for (int i = 0 ; i < NUM_LEDS; i++ ) {
       if (run_mode != one_at_a_time) return;
       memset(leds, 0, NUM_LEDS * 3);
-      switch (j) {
-        case 0: leds[i].r = 255; break;
-        case 1: leds[i].g = 255; break;
-        case 2: leds[i].b = 255; break;
-      }
+      leds[i] = color;
       FastLED.show();
-      delay(15);
+      delay(5);
     }
   }
 }
 
-// Bar effect in red, green or blue color
+// Bar effect in random color
 void runBars() {
-  for (int j = 0; j < 3; j++) {
+  while (true) {
+    CHSV color = CHSV(random(0, 255), random(200, 255), random(200, 255));
     memset(leds, 0, NUM_LEDS * 3);
     for (int i = 0 ; i < NUM_LEDS; i++ ) {
       if (run_mode != bars) return;
-      switch (j) {
-        case 0: leds[i].r = 255; break;
-        case 1: leds[i].g = 255; break;
-        case 2: leds[i].b = 255; break;
-      }
+      leds[i] = color;
       FastLED.show();
-      delay(10);
+      delay(7);
     }
     for (int i = NUM_LEDS - 1 ; i >= 0; i-- ) {
       if (run_mode != bars) return;
-      switch (j) {
-        case 0: leds[i].r = 0; break;
-        case 1: leds[i].g = 0; break;
-        case 2: leds[i].b = 0; break;
-      }
+      leds[i] = CRGB::Black;
       FastSPI_LED.show();
-      delay(1);
+      delay(2);
     }
   }
 }
@@ -201,28 +197,27 @@ void runFire() {
 
 // Run "strobe" effect
 void runStrobe() {
-  for (int i = 0; i <= random(2, 6); i++) {
-    memset(leds, 120, NUM_LEDS * 3);
-    FastLED.show();
-    delay(50);
-    memset(leds, 0, NUM_LEDS * 3);
-    FastLED.show();
-    delay(50);
+  while (true) {
+    CHSV color = CHSV(random(0, 255), random(200, 255), random(200, 255));
+    for (int i = 0; i <= random(2, 6); i++) {
+      FastLED.showColor(color);
+      delay(50);
+      FastLED.showColor(CRGB::Black);
+      delay(50);
+    }
+    if (run_mode != strobe) return;
+    delay(1000);
   }
-  if (run_mode != strobe) return;
-  delay(1000);
 }
 
 // "twinkle" effect.
-// single LED or 40 leds, random color or red
-void runTwinkle(boolean single_led, boolean random_color) {
+void runTwinkle(int max_leds, boolean random_color) {
   int amount = 1;
-  int temp_leds[40];
-  amount = (single_led ? 1 : 40);
+  int temp_leds[max_leds];
   memset(leds, 0, NUM_LEDS * 3);
-  memset(temp_leds, 0, 40 * 2);
+  memset(temp_leds, 0, max_leds * 2);
   while (true) {
-    for (int i = 0; i < amount; i++) {
+    for (int i = 0; i < max_leds; i++) {
       leds[temp_leds[i]] = CRGB::Black;
       int random_led = random(NUM_LEDS);
       temp_leds[i] = random_led;
@@ -236,21 +231,12 @@ void runTwinkle(boolean single_led, boolean random_color) {
       if (random_color) {
         if (run_mode != twinkle_random) return;
       } else {
-        if (run_mode != twinkle && single_led == false) return;
-        if (run_mode != twinkle_single && single_led == true) return;
+        if (run_mode != twinkle && max_leds > 1) return;
+        if (run_mode != twinkle_single && max_leds == 1) return;
       }
     }
     delay(40);
   }
-}
-
-// randomly set one LED white and than turn it off
-void runSparkle() {
-  int led = random(NUM_LEDS);
-  leds[led] = CRGB::White;
-  FastLED.show();
-  leds[led] = CRGB::Black;
-  delay(5);
 }
 
 // turn whole strip almost full white (0x101010)
@@ -258,32 +244,62 @@ void runSparkle() {
 // to original color
 void runSnowSparkle() {
   memset(leds, 0x10, NUM_LEDS * 3);
-  int led = random(NUM_LEDS);
-  leds[led] = CRGB::White;
-  FastLED.show();
-  leds[led] = 0x101010;
-  FastLED.show();
-  delay(200);
+  while (true) {
+    if (run_mode != snow_sparkle) return;
+    int led = random(NUM_LEDS);
+    leds[led] = CRGB::White;
+    FastLED.show();
+    leds[led] = 0x101010;
+    FastLED.show();
+    delay(200);
+  }
 }
 
 // Moving bars
 void runMovingBars() {
   boolean on = false;
   int start = 0;
+  CHSV color = CHSV(random(0, 255), random(200, 255), random(200, 255));
   while (true) {
     memset(leds, 0x00, NUM_LEDS * 3);
-    for (int i = (0-start); i < (NUM_LEDS-start); i++ ) {
+    for (int i = (0 - start); i < (NUM_LEDS - start); i++ ) {
       if (run_mode != moving_bars) return;
       if (i % 25 == 0) on = !on;
-      if (on) leds[i + start] = CRGB::DarkViolet;
+      if (on) leds[i + start] = color;
     }
     FastLED.show();
     start++;
-    if (start == 25) { 
+    if (start == 25) {
       start = 0;
       on = !on;
     }
     delay(10);
+  }
+}
+
+// Cycle rainbow colors on whole strip
+void runRainbowCycle() {
+  while (true) {
+    for (uint8_t hue = 0; hue < 256; hue++) {
+      if (run_mode != rainbow_cycle) return;
+      FastLED.showColor(CHSV(hue, 255, 255));
+      delay(10);
+    }
+  }
+}
+
+// Loop rainbow colors on whole strip
+void runRainbowLoop() {
+  uint8_t hue = 0;
+  while (true) {
+    if (run_mode != rainbow_loop) return;
+    for (int i = 0 ; i < NUM_LEDS; i++ ) {
+      leds[i] = CHSV((hue + i) % 256, 255, 255);
+    }
+    FastLED.show();
+    delay(3);
+    hue++;
+    if (hue > 255) hue = 0;
   }
 }
 
@@ -298,17 +314,21 @@ void loop() {
     case solid_purple: runSingleColor(CRGB::Purple); break;
     case solid_green: runSingleColor(CRGB::Green); break;
     case solid_yellow: runSingleColor(CRGB::Yellow); break;
+    // "halfs"
+    case first_half: runFirstHalf(); break;
+    case second_half: runSecondHalf(); break;
     // effects
     case one_at_a_time: runOneAtATime(); break;
     case bars: runBars(); break;
+    case rainbow_cycle: runRainbowCycle(); break;
+    case rainbow_loop: runRainbowLoop(); break;
     case moving_bars: runMovingBars(); break;
     case fade: runFade(); break;
     case fire: runFire(); break;
     case strobe: runStrobe(); break;
-    case twinkle: runTwinkle(false, false); break;
-    case twinkle_random: runTwinkle(false, true); break;
-    case twinkle_single: runTwinkle(true, false); break;
-    case sparkle: runSparkle(); break;
+    case twinkle: runTwinkle(30, false); break;
+    case twinkle_random: runTwinkle(35, true); break;
+    case twinkle_single: runTwinkle(1, false); break;
     case snow_sparkle: runSnowSparkle(); break;
   }
 }
